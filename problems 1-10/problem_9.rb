6c50e553963ae1d6654a693b14d7eb09a29990f9
Ka_Lenovo <heykamok@gmail.com>
2016-06-12 18:38:33 -0400

    Add idea for problem 9

---
+++ problems 1-10/problem_9.rb
@@ -0,0 +1,16 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+# a**2 + b**2 = c**2
+# For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+#a**2 + b**2 = c**2
+# a + b + c = 1000
+
+# say we break the 1000 into percentages [..25%...50%...75%...100%]
+# a or b cannot exceed 50%. If a or b exceed 50%, then 50% + b != c. 
+# As you cannot divide the remaining 50% between b and c
+
+#Find all a,b pairs under 500 where a pythagorean triplet exist.
