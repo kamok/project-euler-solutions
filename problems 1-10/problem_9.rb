cb7ca59df8081d81fb0467fda44e99bb6555eb4d
Ka Mok <heykamok@gmail.com>
2016-07-26 13:00:06 -0400

    Solve 9!

--- problems 1-10/problem_9.rb
+++ problems 1-10/problem_9.rb
@@ -1,11 +1,11 @@
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
 # a**2 + b**2 = c**2
-# For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**.
+# For example, 3**2 + 4**2 = 5**
+             # 9    + 16   = 25 
 
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
-
 #a**2 + b**2 = c**2
 # a + b + c = 1000
 
@@ -13,33 +13,17 @@
 # a or b cannot exceed 50%. If a or b exceed 50%, then 50% + b != c. 
 # As you cannot divide the remaining 50% between b and c
 
-#Find all a,b pairs under 500 where a pythagorean triplet exist.
+#Find all a, b pairs under 500 where a pythagorean triplet exist.
+include Math
 
-            class GF  
- def initialize  
-  puts 'In GF class'  
- end  
- def gfmethod  
-  puts 'GF method call'  
- end  
-end  
-  
-# class F sub-class of GF  
-class F < GF  
- def initialize  
-  puts 'In F class'  
- end
+a = 1..500
+b = 1..500
 
-def gfmethod
-	puts 'F method call'
-end  
-end  
-  
-# class S sub-class of F  
-class S < F  
- def initialize  
-  puts 'In S class'  
- end  
-end  
-son = S.new  
-son.gfmethod  
+a.each do |a_num|
+  b.each do |b_num|
+    c_num = sqrt(a_num**2 + b_num**2)
+    if c_num % 1 == 0 && a_num + b_num + c_num == 1000
+      p a_num * b_num * c_num
+    end
+  end
+end
