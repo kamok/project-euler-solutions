02980c7d1a355eb0823a105c7da0940f15333fc0
Ka Mok <heykamok@gmail.com>
2016-08-07 00:16:27 -0400

    Not saving the dictionary makes the program faster

--- problems 11-20/problem_14.rb
+++ problems 11-20/problem_14.rb
@@ -12,29 +12,42 @@
 
 # NOTE: Once the chain starts the terms are allowed to go above one million.
 
-def sequence(num)
-  copy = num
-  counter = 1
-  loop do
-    if num.even? == true
-      num /= 2
-    else
-      num = (num * 3) + 1
-    end
-    counter += 1
-    break if num == 1
-  end
-  {copy => counter}
-end
+# Below solution took 27 seconds.
+# def sequence(num)
+#   copy = num
+#   counter = 1
+#   loop do
+#     if num.even? == true
+#       num /= 2
+#     else
+#       num = (num * 3) + 1
+#     end
+#     counter += 1
+#     break if num == 1
+#   end
+#   {copy => counter}
+# end
+# one_million_tries = {}
+# (1..1_000_000).to_enum.each do |num|
+#   hash = sequence(num)
+#   one_million_tries[hash.keys[0]] = hash.values[0]
+#   puts hash
+# end
+# p one_million_tries.max_by { |k,v| v}
 
-one_million_tries = {}
+# One way to still use brute force is by NOT saving the big ass 1 million value dictionary. 
+# Only instansiate one chain at a time, and check the count of the chain against each other.
+# Keep highest one, return the first number of the chain that remains 
+# 
+# solution = []
+# (1..1_000_000).to_enum.each do |num|
+#   current_chain = []
+#   until num <= 1
+#     current_chain << num
+#     num = num.even? ? num / 2 : (num * 3 ) + 1
+#   end 
+#   solution = current_chain if current_chain.length > solution.length
+# end
+# puts solution.first
 
-(1..1_000_000).to_enum.each do |num|
-  hash = sequence(num)
-  one_million_tries[hash.keys[0]] = hash.values[0]
-  puts hash
-end
-
-p one_million_tries.max_by { |k,v| v}
-
-#Time to get humbled by the question's forums!
+#This solution is 15.7 seconds... is .even? faster or % 2 == true
